289128bd4df8580d7e384c8eff79dee7e5f0a6a6
sudkushal <sudarshankushal@gmail.com>
2024-07-26 19:50:51 +0530

    Update _menu.php

--- app/Views/_menu.php
+++ app/Views/_menu.php
@@ -3,7 +3,7 @@
 <nav>
     <ul>
         <li><a href="<?= site_url('/'); ?>" class="<?php if (current_url() == site_url('/')) echo 'active'; ?>">Home</a></li>
-        <li><a href="<?= site_url('leaderboard'); ?>" class="<?php if (current_url() == site_url('leaderboard')) echo 'active'; ?>">Leaderboard</a></li>
+        <!--<li><a href="<?= site_url('leaderboard'); ?>" class="<?php if (current_url() == site_url('leaderboard')) echo 'active'; ?>">Leaderboard</a></li> -->
         <li><a href="<?= site_url('leaderboard100'); ?>" class="<?php if (current_url() == site_url('leaderboard100')) echo 'active'; ?>">100 Days Leaderboard</a></li>
         <li><a href="<?= site_url('analytics'); ?>" class="<?php if (current_url() == site_url('analytics')) echo 'active'; ?>">Analytics</a></li>
     </ul>
