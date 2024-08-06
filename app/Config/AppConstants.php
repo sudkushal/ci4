38738108ea9cbc5b908e9b272639c13fdd682307
sudkushal <sudarshankushal@gmail.com>
2024-08-06 18:56:44 +0530

    Update AppConstants.php

--- app/Config/AppConstants.php
+++ app/Config/AppConstants.php
@@ -3,7 +3,7 @@ namespace App\Config;
 
 class AppConstants
 {
-    public const CHALLENGE_START_DATE = '2024-07-11';
+    public const CHALLENGE_START_DATE = '2024-08-15';
     public const CHALLENGE_END_DATE   = '2024-11-30';
     // ...other constants if you need them
 }
