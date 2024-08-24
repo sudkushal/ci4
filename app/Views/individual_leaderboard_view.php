e58544857ff1bbcdffc6ce1a44693b88456805b7
sudkushal <sudarshankushal@gmail.com>
2024-08-24 09:40:39 +0530

    Update individual_leaderboard_view.php

--- app/Views/individual_leaderboard_view.php
+++ app/Views/individual_leaderboard_view.php
@@ -10,34 +10,37 @@
 
     <div class="row">
         <div class="col-12">
-            <!-- Bootstrap Responsive Table with DataTables -->
-            <table id="individual-stats-table" class="table table-striped table-bordered table-responsive-sm">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Max Distance</th>
-                        <th>Total Distance</th>
-                        <th>Total Time</th>
-                        <th>Avg Activities/Week</th>
-                        <th>Avg Dist/Week</th>
-                        <th>Avg Time/Week</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $tableData = json_decode($tableData, true); ?>
-                    <?php foreach ($tableData as $row) : ?>
+            <div class="table-responsive">
+                <table id="individual-stats-table" class="table table-striped table-bordered table-responsive-sm">
+                    <thead>
                         <tr>
-                            <td><?= htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars(number_format($row['max_distance'], 2), ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars(number_format($row['total_distance'], 2), ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars(number_format($row['total_time'], 2), ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars($row['activities_per_week'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars(number_format($row['avg_distance_per_week'], 2), ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?= htmlspecialchars(number_format($row['avg_time_per_week'], 2), ENT_QUOTES, 'UTF-8'); ?></td>
+                            <th>Name</th>
+                            <th>Total Activities</th>
+                            <th>Total Distance (km)</th>
+                            <th>Total Time (hours)</th>
+                            <th>Activities/Week</th>
+                            <th>Avg Dist/Week (km)</th>
+                            <th>Avg Time/Week (hours)</th>
+                            <th>Personal Best Distance (km)</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php $tableData = json_decode($tableData, true); ?>
+                        <?php foreach ($tableData as $row) : ?>
+                            <tr>
+                                <td><?= htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['total_activities'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['total_distance_km'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['total_time_hours'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['activities_per_week'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['avg_distance_per_week_km'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['avg_time_per_week_hours'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                <td><?= htmlspecialchars($row['personal_best_distance_km'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
